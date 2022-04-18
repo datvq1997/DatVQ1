9a5399f221e68ca4d8bfbea8dec7c0fac37d4daf
datvq1997 <103942805+datvq1997@users.noreply.github.com>
2022-04-18 20:44:23 +0700

    Add files via upload

---
+++ DatVQ1_Assignment1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+bool IsPalindrome(int x)
+{
+	int buf = 0;
+	int ReX = 0;
+	int OrgX = x;
+ 	do {
+		buf = OrgX % 10;
+		OrgX = OrgX / 10;
+		ReX = ReX * 10 + buf;
+	} while (OrgX != 0);
+
+	if (ReX == x) {
+		return true;
+	}
+	return false;
+}
+
+/*int main()
+{
+	int x; 
+
+	cout << "Number : ";
+	cin >> x;
+	if (IsPalindrome(x) == true) {
+		cout << "\n" << x << " is Palindrome \n";
+	}
+	else {
+		cout << "\n" << x << " isn't Palindrome \n";
+	}
+}*/
+
+
